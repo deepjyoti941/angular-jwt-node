615d85bd6265b0b36484867a2016fd82a9382cee
deepjyoti khakhlary <deepjyoti941@gmail.com>
2014-11-21 00:04:42 +0530

    initializing app

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name jwtAngularNodeApp
+ * @description
+ * # jwtAngularNodeApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('jwtAngularNodeApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name jwtAngularNodeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the jwtAngularNodeApp
+ */
+angular.module('jwtAngularNodeApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
