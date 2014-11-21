9219d80ef37239ae5c6e6ca7cba20e8da12494c4
deepjyoti khakhlary <deepjyoti941@gmail.com>
2014-11-22 00:15:31 +0530

    ui router configuration

---
+++ app/scripts/app.config.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular.module('jwtAngularNodeApp').config(function ($urlRouterProvider, $stateProvider) {
+
+  $urlRouterProvider.otherwise('/');
+
+  $stateProvider
+
+  .state('main', {
+    url: '/',
+    templateUrl: '/views/main.html'
+  })
+
+  .state('register', {
+    url: '/register',
+    templateUrl: '/views/register.html'
+  });
+});
