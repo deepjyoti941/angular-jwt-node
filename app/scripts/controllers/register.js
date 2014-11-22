0a9537ce1714b94c0bc7006c20ec7cd3df4cb113
deepjyoti khakhlary <deepjyoti941@gmail.com>
2014-11-22 23:01:15 +0530

    alert service

--- app/scripts/controllers/register.js
+++ app/scripts/controllers/register.js
@@ -1,16 +1,16 @@
 'use strict';
 
 angular.module('jwtAngularNodeApp')
-  .controller('RegisterCtrl', function ($scope, $http) {
+  .controller('RegisterCtrl', function ($scope, $rootScope, $http, alert) {
     $scope.submit = function () {
       var url = '/';
       var user = {};
       $http.post(url, user)
         .success(function (res) {
-          console.log("good");
+          alert('success', 'OK!', 'You are now registered');
         })
         .error(function (err) {
-          console.log("bad");
+          alert('warning', 'Opps!', 'Could not register');
         });
     };
   });
