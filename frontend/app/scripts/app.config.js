7ab1675590f9ec3ba897f9096032699a686389d0
deepjyoti khakhlary <deepjyoti941@gmail.com>
2014-11-24 22:45:48 +0530

    login view

--- frontend/app/scripts/app.config.js
+++ frontend/app/scripts/app.config.js
@@ -17,6 +17,12 @@ angular.module('jwtAngularNodeApp').config(function ($urlRouterProvider, $stateP
     controller: 'RegisterCtrl'
   })
 
+  .state('login', {
+    url: '/login',
+    templateUrl: '/views/login.html',
+    controller: 'LoginCtrl'
+  })
+
   .state('jobs', {
     url: '/jobs',
     templateUrl: '/views/jobs.html',
---
+++ frontend/app/scripts/controllers/login.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name jwtAngularNodeApp.controller:LoginCtrl
+ * @description
+ * # LoginCtrl
+ * Controller of the jwtAngularNodeApp
+ */
+angular.module('jwtAngularNodeApp')
+  .controller('LoginCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
