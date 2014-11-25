ea63fa14e886c064c641ad926603fbf9beb860c2
deepjyoti khakhlary <deepjyoti941@gmail.com>
2014-11-26 00:34:59 +0530

    Register Auth Service

--- frontend/app/scripts/controllers/register.js
+++ frontend/app/scripts/controllers/register.js
@@ -1,17 +1,11 @@
 'use strict';
 
 angular.module('jwtAngularNodeApp')
-  .controller('RegisterCtrl', function ($scope, $rootScope, $http, alert, authToken, API_URL) {
+  .controller('RegisterCtrl', function ($scope, alert, auth) {
     $scope.submit = function () {
-      var url = API_URL + 'register';
-      var user = {
-        email: $scope.email,
-        password: $scope.password
-      };
-      $http.post(url, user)
+      auth.register($scope.email, $scope.password)
         .success(function (res) {
           alert('success', 'Account Created!', 'Welcome, ' + res.user.email + ' !');
-          authToken.setToken(res.token);
         })
         .error(function (err) {
           alert('warning', 'Some went wrong!', err.message);
--- frontend/app/scripts/services/auth.js
+++ frontend/app/scripts/services/auth.js
@@ -1,15 +1,25 @@
 'use strict';
 
 angular.module('jwtAngularNodeApp')
-  .service('auth', function ($http, API_URL, authToken) {
-    var url = API_URL + 'login';
+  .service('auth', function ($http, API_URL, authToken, $state) {
+
+    function authSuccessfull(res) {
+      authToken.setToken(res.token);
+      $state.go('main');
+    }
 
     this.login = function (email, password) {
-      return $http.post(url, {
+      return $http.post(API_URL + 'login', {
         email: email,
         password: password
-      }).success(function (res) {
-        authToken.setToken(res.token);
-      });
+      }).success(authSuccessfull);
     }
+
+    this.register = function (email, password) {
+      return $http.post(API_URL + 'register', {
+        email: email,
+        password: password
+      }).success(authSuccessfull);
+    }
+
   });
