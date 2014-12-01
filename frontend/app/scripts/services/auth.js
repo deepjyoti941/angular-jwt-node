fd7be18ae3ae7310684952bf1d18f3750d3d732e
deepjyoti khakhlary <deepjyoti941@gmail.com>
2014-12-01 21:25:27 +0530

    Receiving Authorization Code in Main Window

--- frontend/app/scripts/services/auth.js
+++ frontend/app/scripts/services/auth.js
@@ -30,7 +30,15 @@ angular.module('jwtAngularNodeApp')
     this.googleAuth = function () {
       var url = "https://accounts.google.com/o/oauth2/auth?" + urlBuilder.join('&');
       var options = "width=500, height=500, left="+ ($window.outerWidth - 500) / 2 + ",top="+ ($window.outerWidth - 500) / 2.5;
+      
       $window.open(url, '', options);
+      $window.focus();
+
+      $window.addEventListener('message', function(event) {
+        if (event.origin === $window.location.origin) {
+          console.log(event.data);
+        };
+      })
     }
 
   });
