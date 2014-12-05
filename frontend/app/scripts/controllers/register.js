3d97e8f855bfc32a3ef9a9339cfe85b9605ce28e
deepjyoti khakhlary <deepjyoti941@gmail.com>
2014-12-05 23:50:48 +0530

    local register with satellizer

--- frontend/app/scripts/controllers/register.js
+++ frontend/app/scripts/controllers/register.js
@@ -1,13 +1,13 @@
 'use strict';
 
 angular.module('jwtAngularNodeApp')
-  .controller('RegisterCtrl', function ($scope, alert, auth) {
+  .controller('RegisterCtrl', function ($scope, alert, $auth) {
     $scope.submit = function () {
-      auth.register($scope.email, $scope.password)
-        .success(function (res) {
-          alert('success', 'Account Created!', 'Welcome, ' + res.user.email + ' !');
+      $auth.signup({email: $scope.email, password: $scope.password})
+        .then(function (res) {
+          alert('success', 'Account Created!', 'Welcome, ' + res.data.user.email + ' !');
         })
-        .error(function (err) {
+        .catch(function (err) {
           alert('warning', 'Some went wrong!', err.message);
         });
     };
